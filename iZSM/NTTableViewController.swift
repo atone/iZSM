6693c6d81828dc6853ae84732b71b20421222d3d
Naitong Yu <yunaitong@gmail.com>
2017-07-16 17:16:00 +0800

    代码格式化

--- iZSM/NTTableViewController.swift
+++ iZSM/NTTableViewController.swift
@@ -29,12 +29,15 @@ class NTTableViewController: UITableViewController {
     func changeColor() {
         tableView.backgroundColor = (tableView.style == .grouped) ? AppTheme.shared.lightBackgroundColor : AppTheme.shared.backgroundColor
         tableView.tintColor = AppTheme.shared.tintColor
+        
         tableView.mj_header?.backgroundColor = AppTheme.shared.backgroundColor
         (tableView.mj_header as? MJRefreshNormalHeader)?.activityIndicatorViewStyle = setting.nightMode ? UIActivityIndicatorViewStyle.white : UIActivityIndicatorViewStyle.gray
         (tableView.mj_header as? MJRefreshNormalHeader)?.stateLabel.textColor = AppTheme.shared.lightTextColor
+        
         tableView.mj_footer?.backgroundColor = AppTheme.shared.backgroundColor
         (tableView.mj_footer as? MJRefreshAutoNormalFooter)?.activityIndicatorViewStyle = setting.nightMode ? UIActivityIndicatorViewStyle.white : UIActivityIndicatorViewStyle.gray
         (tableView.mj_footer as? MJRefreshAutoNormalFooter)?.stateLabel.textColor = AppTheme.shared.lightTextColor
+        
         tableView.reloadData()
     }
     

3aa30204ec1b45f5d588e1504d0c8342f564c200
Naitong Yu <yunaitong@gmail.com>
2017-07-17 22:26:27 +0800

    将标题也挪到textview中

--- iZSM/FullscreenContentViewController.swift
+++ iZSM/FullscreenContentViewController.swift
@@ -13,32 +13,17 @@ class FullscreenContentViewController: NTViewController {
     
     var article: SMArticle?
     
-    private let titleLabel = UILabel()
     private let contentTextView = UITextView()
     
     private func setupUI() {
-        view.addSubview(titleLabel)
         view.addSubview(contentTextView)
-        titleLabel.backgroundColor = UIColor.clear
-        titleLabel.setContentHuggingPriority(UILayoutPriorityDefaultHigh, for: .vertical)
-        titleLabel.setContentCompressionResistancePriority(UILayoutPriorityDefaultHigh, for: .vertical)
-        contentTextView.setContentHuggingPriority(UILayoutPriorityDefaultLow, for: .vertical)
-        contentTextView.setContentCompressionResistancePriority(UILayoutPriorityDefaultLow, for: .vertical)
-        titleLabel.numberOfLines = 0
-        titleLabel.lineBreakMode = .byWordWrapping
-        titleLabel.textAlignment = .center
-        titleLabel.snp.makeConstraints { (make) in
-            make.leading.equalTo(view.snp.leadingMargin)
-            make.trailing.equalTo(view.snp.trailingMargin)
-            make.top.equalTo(view.snp.top).offset(20)
-        }
         contentTextView.backgroundColor = UIColor.clear
         contentTextView.isEditable = false
         contentTextView.dataDetectorTypes = [.link, .phoneNumber]
         contentTextView.snp.makeConstraints { (make) in
             make.leading.equalTo(view.snp.leadingMargin)
             make.trailing.equalTo(view.snp.trailingMargin)
-            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.top.equalTo(view.snp.top).offset(20)
             make.bottom.equalTo(view.snp.bottomMargin)
         }
         updateContent()
@@ -47,16 +32,54 @@ class FullscreenContentViewController: NTViewController {
     private func updateContent() {
         view.backgroundColor = AppTheme.shared.backgroundColor
         if let article = article {
-            titleLabel.text = article.subject
+            let fullArticle = NSMutableAttributedString()
+            
             let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title2)
-            titleLabel.font = UIFont.boldSystemFont(ofSize: descriptor.pointSize)
-            titleLabel.textColor = AppTheme.shared.textColor
+            let titleFont = UIFont.boldSystemFont(ofSize: descriptor.pointSize)
+            let titleColor = AppTheme.shared.textColor
+            let titleParagraphStyle = NSMutableParagraphStyle()
+            titleParagraphStyle.alignment = .center
+            titleParagraphStyle.lineBreakMode = .byWordWrapping
+            titleParagraphStyle.maximumLineHeight = titleFont.pointSize
+            titleParagraphStyle.minimumLineHeight = titleFont.pointSize
+            titleParagraphStyle.lineSpacing = titleFont.pointSize / 4
+            let title = NSAttributedString(string: article.subject,
+                                           attributes: [NSParagraphStyleAttributeName: titleParagraphStyle,
+                                                        NSFontAttributeName: titleFont,
+                                                        NSForegroundColorAttributeName: titleColor])
+            fullArticle.append(title)
+            fullArticle.appendString("\n")
+            
+            let subtitleText = "作者: \(article.authorID) 时间: \(article.timeString)"
+            let subtitleFont = UIFont.preferredFont(forTextStyle: .subheadline)
+            let subtitleColor = AppTheme.shared.lightTextColor
+            let subtitleParagraphStyle = NSMutableParagraphStyle()
+            subtitleParagraphStyle.alignment = .center
+            subtitleParagraphStyle.lineBreakMode = .byWordWrapping
+            subtitleParagraphStyle.maximumLineHeight = subtitleFont.pointSize
+            subtitleParagraphStyle.minimumLineHeight = subtitleFont.pointSize
+            subtitleParagraphStyle.paragraphSpacing = 10
+            subtitleParagraphStyle.paragraphSpacingBefore = 5
+            let subtitle = NSAttributedString(string: subtitleText,
+                                              attributes: [NSParagraphStyleAttributeName: subtitleParagraphStyle,
+                                                           NSFontAttributeName: subtitleFont,
+                                                           NSForegroundColorAttributeName: subtitleColor])
+            fullArticle.append(subtitle)
+            fullArticle.appendString("\n")
             
             let attributedBody = AppSetting.shared.nightMode ? article.attributedDarkBody : article.attributedBody
+            let font = UIFont.preferredFont(forTextStyle: .title3)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = font.pointSize / 4
+            paragraphStyle.alignment = .natural
+            paragraphStyle.minimumLineHeight = font.pointSize
+            paragraphStyle.maximumLineHeight = font.pointSize
+            paragraphStyle.lineBreakMode = .byWordWrapping
             let mutableAttributedBody = NSMutableAttributedString(attributedString: attributedBody)
-            mutableAttributedBody.setFont(UIFont.preferredFont(forTextStyle: .title3),
-                                          range: NSMakeRange(0, mutableAttributedBody.string.characters.count))
-            contentTextView.attributedText = mutableAttributedBody
+            mutableAttributedBody.addAttributes([NSParagraphStyleAttributeName: paragraphStyle, NSFontAttributeName: font],
+                                                range: NSMakeRange(0, mutableAttributedBody.string.characters.count))
+            fullArticle.append(mutableAttributedBody)
+            contentTextView.attributedText = fullArticle
         }
     }
     

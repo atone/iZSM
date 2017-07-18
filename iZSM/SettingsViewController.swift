041078986872ed4aa27d4efd00238dafbdff194d
Naitong Yu <yunaitong@gmail.com>
2017-07-19 06:23:08 +0800

    修复了清除缓存按钮失效的bug

--- iZSM/SettingsViewController.swift
+++ iZSM/SettingsViewController.swift
@@ -199,7 +199,7 @@ class SettingsViewController: NTTableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath == IndexPath(row: 0, section: 2) {
+        if indexPath == IndexPath(row: 0, section: 3) {
             tableView.deselectRow(at: indexPath, animated: true)
             SVProgressHUD.show()
             DispatchQueue.global().async {

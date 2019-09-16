9ee7bcb226c62645d37e0932510f09ea91024739
Naitong Yu <yunaitong@gmail.com>
2019-09-17 01:30:01 +0800

    add trim support for NSMutableAttributedString

--- iZSM/SmthStructs.swift
+++ iZSM/SmthStructs.swift
@@ -207,7 +207,7 @@ struct SMArticle {
                 self.quotedAttributedRange.append(trimRange)
             }
         }
-        
+        attributeText.trimCharactersInSet(charSet: .whitespacesAndNewlines)
         return attributeText
     }
     
@@ -450,3 +450,20 @@ struct SMMember {
     var userID: String
 }
 
+extension NSMutableAttributedString {
+     public func trimCharactersInSet(charSet: CharacterSet) {
+        var range = (string as NSString).rangeOfCharacter(from: charSet)
+
+         // Trim leading characters from character set.
+         while range.length != 0 && range.location == 0 {
+            replaceCharacters(in: range, with: "")
+            range = (string as NSString).rangeOfCharacter(from: charSet)
+         }
+        // Trim trailing characters from character set.
+        range = (string as NSString).rangeOfCharacter(from: charSet, options: .backwards)
+        while range.length != 0 && NSMaxRange(range) == length {
+            replaceCharacters(in: range, with: "")
+            range = (string as NSString).rangeOfCharacter(from: charSet, options: .backwards)
+        }
+    }
+}
